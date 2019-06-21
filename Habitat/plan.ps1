936a934d6414c6e33c752da686070229b2f4e551
Jeff Brimager <JBrimager@gmail.com>
2019-06-21 15:21:04 -0500

    running config
    Signed-off-by: Jeff Brimager <JBrimager@gmail.com>

--- Habitat/plan.ps1
+++ Habitat/plan.ps1
@@ -1,10 +1,10 @@
 $pkg_name="tomcat8"
-$pkg_origin="core"
+$pkg_origin="winhab"
 $pkg_version="8.5.41"
 $pkg_description="An open source implementation of the Java Servlet, JavaServer Pages, Java Expression Language and Java WebSocket technologies."
 $pkg_upstream_url="https://tomcat.apache.org/"
-$pkg_license=@("Apache 2.0")
-$pkg_deps=@("winhab/corretto_jre8")
+$pkg_license=@("Apache-2.0")
+$pkg_deps=@("core/corretto8")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="http://apache.spinellicreations.com/tomcat/tomcat-8/v$pkg_version/bin/apache-tomcat-$pkg_version-windows-x64.zip"
 $pkg_shasum="117230aac76ecc1e9d04aff4e312dd9ff9ccb20d6246d0eef8199eec073f9778"
